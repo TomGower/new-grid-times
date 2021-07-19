1479738363083a51a31b883224ad4c432eb3a9be
TomGower <tom.gower@gmail.com>
2021-07-19 12:41:16 -0500

    Exercise 1 work

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,9 +8,16 @@ import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
 import Button from '../Button';
 
-const Header = () => {
+const Header = () => (
+  <>
+    <MobileHeader />
+    <DesktopHeader />
+  </>
+)
+
+const MobileHeader = () => {
   return (
-    <header>
+    <MobileWrapper>
       <SuperHeader>
         <Row>
           <ActionGroup>
@@ -31,14 +38,60 @@ const Header = () => {
       <MainHeader>
         <Logo />
       </MainHeader>
-    </header>
+    </MobileWrapper>
   );
 };
+
+const DesktopHeader = () => {
+  return (
+    <DesktopWrapper>
+      <DesktopSuperHeader>
+        <ActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroup>
+        <Logo />
+        <SubscribeWrapper>
+          <Button>Subscribe</Button>
+          <SubscriberText>Already a subscriber?</SubscriberText>
+        </SubscribeWrapper>
+      </DesktopSuperHeader>
+    </DesktopWrapper>
+  );  
+}
+
+const MobileWrapper = styled.header`
+  display: inherit;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+
+const DesktopWrapper = styled(MaxWidthWrapper)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: inherit;
+    margin-top: 16px;
+    margin-bottom: 80px;
+  }
+`;
 
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+`;
+
+const DesktopSuperHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -49,7 +102,7 @@ const Row = styled(MaxWidthWrapper)`
 const ActionGroup = styled.div`
   display: flex;
   gap: 24px;
-
+ 
   /*
     FIX: Remove the inline spacing that comes with
     react-feather icons.
@@ -65,6 +118,20 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+`;
+
+const SubscribeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 16px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SubscriberText = styled.div`
+  font-style: italic;
+  color: var(--color-gray-900);
+  text-decoration: underline;
 `;
 
 export default Header;
