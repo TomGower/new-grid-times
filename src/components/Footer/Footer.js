6c2a6d993cb1724ac5166573d1c2486f413d0672
TomGower <tom.gower@gmail.com>
2021-07-20 15:32:25 -0500

    Exercise 5 completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Twitter, Facebook } from 'react-feather';
 import styled from 'styled-components/macro';
+import { QUERIES } from '../../constants';
 import MaxWidthWrapper from '../MaxWidthWrapper';
 
 import VisuallyHidden from '../VisuallyHidden';
@@ -13,7 +14,7 @@ const Footer = () => {
           <nav>
             <TopNavList>
               <li>
-                <a href="/about">About</a>
+                <a href="/about">About us</a>
               </li>
               <li>
                 <a href="/press">Press Releases</a>
@@ -144,6 +145,12 @@ const TopRow = styled.div`
   font-size: 0.875rem;
   border-bottom: 1px solid var(--color-gray-700);
   padding: 24px 0;
+
+  @media ${QUERIES.tabletOnly} {
+    flex-direction: row;
+    justify-content: center;
+    gap: 48px;
+  }
 `;
 
 const Social = styled.div`
@@ -170,6 +177,13 @@ const MainNavArea = styled.div`
   gap: 32px;
   padding: 32px 0 48px;
   text-align: center;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(175px, 1fr));
+    gap: 32px 80px;
+    text-align: revert;
+  }
 `;
 
 const MainNavHeading = styled.h2`
@@ -196,6 +210,10 @@ const Subfooter = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media ${QUERIES.laptopAndUp} {
+    align-items: flex-start;
+  }
 `;
 
 const Logo = styled.a`
